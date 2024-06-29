40fa7d11e7af33339408a36d163d987b4e176626
arty <morozartem9@gmail.com>
2024-06-29 13:54:55 +0300

    git all dist files just for demo

---
+++ dist/esm/utils.d.ts
@@ -0,0 +1,14 @@
+/********************************
+     Encoding/Decoding Utils
+********************************/
+export declare function randomChallenge(): string;
+export declare function toBuffer(txt: string): ArrayBuffer;
+export declare function parseBuffer(buffer: ArrayBuffer): string;
+export declare function isBase64url(txt: string): boolean;
+export declare function toBase64url(buffer: ArrayBuffer): string;
+export declare function parseBase64url(txt: string): ArrayBuffer;
+export declare function parseBase64UrlLegacy(txt: string): ArrayBuffer;
+export declare function toBase64urlLegacy(buffer: ArrayBuffer): string;
+export declare function sha256(buffer: ArrayBuffer, externalCrypto?: Crypto): Promise<ArrayBuffer>;
+export declare function bufferToHex(buffer: ArrayBuffer): string;
+export declare function concatenateBuffers(buffer1: ArrayBuffer, buffer2: ArrayBuffer): Uint8Array;
